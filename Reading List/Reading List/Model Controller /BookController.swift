dc639efe79af0f41fda489a6bf1cf999a7f031d4
Your Name Here <youremailhere>
2020-04-24 11:03:05 -0400

    Create a computed property called readingListURL: URL?.

--- Reading List/Reading List/Model Controller /BookController.swift
+++ Reading List/Reading List/Model Controller /BookController.swift
@@ -9,4 +9,24 @@
 import Foundation
 class BookController: Codable {
     var books: [Book] = []
+    
+    
+    // CRUD
+
+
+    // #3 Create a filename string for the plist, such as "ReadingList.plist" -- Not understading this task? 
+    private var readingListURL: URL? {
+        let fileManager = FileManager.default
+        guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask) .first else { return nil }
+        return documentDirectory.appendingPathComponent("books.plist") // Am I suppose to use ReadingList.plist or books?
+    }
+
+
+    func saveToPersistentStore() {
+        
+    }
+    
+    
 }
+
+
