2a5c07a8a010138706cd1b9a4f0d62b739cbb6cc
Your Name Here <youremailhere>
2020-04-24 21:27:58 -0400

    Added function called saveToPersistentStore which is responsible fo saving any changes

--- Reading List/Reading List/Model Controller /BookController.swift
+++ Reading List/Reading List/Model Controller /BookController.swift
@@ -14,16 +14,25 @@ class BookController: Codable {
     // CRUD
 
 
-    // #3 Create a filename string for the plist, such as "ReadingList.plist" -- Not understading this task? 
+    // #3 Create a filename string for the plist, such as "ReadingList.plist" -- Not understading this task?
     private var readingListURL: URL? {
         let fileManager = FileManager.default
         guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask) .first else { return nil }
         return documentDirectory.appendingPathComponent("books.plist") // Am I suppose to use ReadingList.plist or books?
     }
 
-
-    func saveToPersistentStore() {
+// Inside of a do-try-catch block create a constant called booksData. Using the encode(value: ...) function of the property list encoder, encode the books array into Data.
+    private func saveToPersistentStore() {
+        guard let booksURL = readingListURL else { return }
         
+        
+        do {
+            let booksEncoder = PropertyListEncoder()
+            let booksData = try booksEncoder.encode(books)
+            try booksData.write(to: booksURL)
+        } catch {
+            NSLog("Error saving Books List data: \(error)")
+        }
     }
     
     
