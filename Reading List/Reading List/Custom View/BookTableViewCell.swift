37d7aa1d1ca8d660f981913e4df3833e34d4402f
Your Name Here <youremailhere>
2020-04-25 18:12:19 -0400

    Complete part2

---
+++ Reading List/Reading List/Custom View/BookTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Sammy Alvarado on 4/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    @IBOutlet weak var bootTitleLabel: UILabel!
+    @IBOutlet weak var checkOnOfflabel: UIButton!
+    
+    @IBAction func checkOnOffButton(_ sender: Any) {
+       }
+    /*
+     override func awakeFromNib() {
+         super.awakeFromNib()
+         // Initialization code
+     }
+
+     override func setSelected(_ selected: Bool, animated: Bool) {
+         super.setSelected(selected, animated: animated)
+
+         // Configure the view for the selected state
+     }
+     */
+    
+   
+    
+}
--- Reading List/Reading List/Model Controller /BookController.swift
+++ Reading List/Reading List/Model Controller /BookController.swift
@@ -8,11 +8,35 @@
 
 import Foundation
 class BookController: Codable {
+    init() {
+        self.loadFromPersistentStore()
+    }
+    
+    
     var books: [Book] = []
     
     
     // CRUD
-
+    // Step 6. Create
+    @discardableResult func createBook(with title: String, reasonToRead: String, hasBeenRead: Bool = false) -> Book {
+        let book = Book(title: title, reasonToRead: reasonToRead)
+        books.append(book)
+        self.saveToPersistentStore()
+        return book
+    }
+    
+//    func bookDataUpdate() {
+//        guard let bookDelegate = b
+//    }
+    
+    /*
+     Add a "Delete" method that passes in a Book object as a parameter, and removes it from the books array.
+     We'll need two "Update" methods:
+     One is to update a Book object's hasBeenRead property. Call it updateHasBeenRead(for book: Book). It should simply swap the hasBeenRead value from false to true and vice-versa.
+     The other is to edit the Book's title and/or reasonToRead properties.
+     */
+    
+    
 
     // Step 3. Persistence Functions
     private var readingListURL: URL? {
